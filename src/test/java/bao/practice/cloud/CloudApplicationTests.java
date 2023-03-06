13078c9e7a70d5c74ec64d8259135ec01eda2610
Pham Vu Thien Bao <97822939+BaoPhamm@users.noreply.github.com>
2023-03-06 21:29:12 +0700

    Initial project

---
+++ src/test/java/bao/practice/cloud/CloudApplicationTests.java
@@ -0,0 +1,13 @@
+package bao.practice.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
