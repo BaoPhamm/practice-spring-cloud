a787109e07d5024f5b397269c3b9781898f0f65e
Pham Vu Thien Bao <97822939+BaoPhamm@users.noreply.github.com>
2023-03-13 22:05:11 +0700

    add configserver

---
+++ configserver/src/test/java/bao/practice/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package bao.practice.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
